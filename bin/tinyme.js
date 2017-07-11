6dd9cd6ffd5743169771463ed4a730480a1fcbca
Patrick Ferreira <patrick.ferreira@possible.com>
2017-07-11 15:20:55 -0300

    Add npm global modules path prefix to env file

--- bin/tinyme.js
+++ bin/tinyme.js
@@ -1,17 +1,18 @@
 #! /usr/bin/env node
 const tinyme = require('./../src/index');
 const program = require('commander');
+const globalModulesPath = require('global-modules');
 const { config } = require('dotenv');
 const { writeFileSync } = require('fs');
 
-config({ path: 'variables.env' });
+config({ path: `${globalModulesPath}/tinyme/variables.env` });
 
 program
   .arguments('<path> [api_key]')
   .usage('<path> [api_key]')
   .action((path, cliApiKey) => {
     if (typeof cliApiKey !== 'undefined') {
-      writeFileSync('variables.env', `TINYME_API_KEY="${cliApiKey}"`);
+      writeFileSync(`${globalModulesPath}/tinyme/variables.env`, `TINYME_API_KEY="${cliApiKey}"`);
     }
 
     const apiKey = cliApiKey || process.env.TINYME_API_KEY || undefined;
